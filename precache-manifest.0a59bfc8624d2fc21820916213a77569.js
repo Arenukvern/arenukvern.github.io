0e3ed5311ede7155ed236806e2d2e01c216897dd
Arenukvern <arenukvern@gmail.com>
2021-06-18 10:52:23 +0000

    update

---
+++ precache-manifest.0a59bfc8624d2fc21820916213a77569.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f34754cbc0b82a4bee69",
+    "url": "/css/app.580145b0.css"
+  },
+  {
+    "revision": "2fb35a0e4e6853f9f2000857e1ca4d1f",
+    "url": "/img/home.svg"
+  },
+  {
+    "revision": "e1ef61aebb53c062395816308619576c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "065253b3474062626a19",
+    "url": "/js/about.44a85e05.js"
+  },
+  {
+    "revision": "f34754cbc0b82a4bee69",
+    "url": "/js/app.df0745ea.js"
+  },
+  {
+    "revision": "bb3d83b93ff405740284",
+    "url": "/js/chunk-vendors.ce2d49c8.js"
+  },
+  {
+    "revision": "f0ff4409007a35dd61f4e8ae9e38d456",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
